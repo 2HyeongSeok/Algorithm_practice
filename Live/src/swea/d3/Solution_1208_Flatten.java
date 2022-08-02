949415bb42778a0beafca4f537d468c56910cbc1
2HyeongSeok <rhfemsqpf90@naver.com>
2022-08-02 09:43:44 +0900

    feat: Add SWEA-1208-Java

---
+++ Live/src/swea/d3/Solution_1208_Flatten.java
@@ -0,0 +1,43 @@
+package swea.d3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution_1208_Flatten {
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+
+		for(int test_case = 1; test_case <= 10; test_case++){
+			int dumpNum = Integer.parseInt(br.readLine());
+			int[] dumpArr = new int[100];
+			String[] line = br.readLine().split(" ");
+			for(int i = 0; i < 100; i++) {
+				dumpArr[i] = Integer.parseInt(line[i]);
+			}
+			sb.append("#").append(test_case).append(" ");
+			dump(dumpArr, dumpNum);
+			
+			
+		}
+
+		System.out.println(sb);
+	}
+	
+	static void dump(int[] dumpArr, int dumpNum) {
+		Arrays.sort(dumpArr);
+		// 기저조건
+		if( dumpArr[dumpArr.length - 1] - dumpArr[0] <= 1 || dumpNum == 0) {
+			sb.append(dumpArr[dumpArr.length - 1] - dumpArr[0]).append("\n");
+			return;
+		}
+		dumpArr[dumpArr.length - 1]--;
+		dumpArr[0]++;
+		dump(dumpArr, dumpNum - 1);
+	}
+}
+
---
+++ Study/src/algo_study/Main_17829_222풀링.java
@@ -0,0 +1,5 @@
+package algo_study;
+
+public class Main_17829_222풀링 {
+
+}
