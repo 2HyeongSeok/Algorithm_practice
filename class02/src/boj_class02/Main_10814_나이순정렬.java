4f74bdd5b7011acfc84cedef7ce970cdab59934d
2HyeongSeok <rhfemsqpf90@naver.com>
2022-08-03 14:54:30 +0900

    feat: Fix BOJ-10814-Java

--- class02/src/boj_class02/Main_10814_나이순정렬.java
+++ class02/src/boj_class02/Main_10814_나이순정렬.java
@@ -8,9 +8,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-import javax.print.attribute.HashAttributeSet;
-
-
 public class Main_10814_나이순정렬 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -41,35 +38,6 @@ public class Main_10814_나이순정렬 {
 				}
 			}
 		}
-		
-//		// 첫 사람
-//		String[] line = br.readLine().split(" ");
-//		age.add(Integer.parseInt(line[0]));
-//		name.add(line[1]);
-//		
-//		for(int i = 1; i < N; i++) {
-//			// 두번째부터 N번째 사람
-//			line = br.readLine().split(" ");
-//			
-//			int thisAge = Integer.parseInt(line[0]);
-//			String thisName = line[1];
-//			
-//			int len = age.size();
-//			for(int k = 0; k < len; k++) {
-//				if(thisAge < age.get(k)) {
-//					age.add(k, thisAge);
-//					name.add(k, thisName);
-//					break;
-//				}
-//				if(k == len - 1) {
-//					age.add(thisAge);
-//					name.add(thisName);
-//				}
-//			}
-//		}
-//		for(int i = 0; i < N; i++) {
-//			sb.append(age.get(i)).append(" ").append(name.get(i)).append("\n");
-//		}
 		
 		System.out.println(sb);
 	}
