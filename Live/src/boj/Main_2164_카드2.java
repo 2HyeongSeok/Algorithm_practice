178fe2d3822c89045fb20cbe39c2f81c08686eac
2HyeongSeok <rhfemsqpf90@naver.com>
2022-08-04 18:14:32 +0900

    feat: Fix BOJ-2164-Java

---
+++ Live/src/boj/Main_2164_카드2.java
@@ -0,0 +1,26 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main_2164_카드2 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		Queue<Integer> queue = new ArrayDeque<Integer>();
+		
+		for(int i = 1; i <= N; i++)
+			queue.offer(i);
+		
+		while(queue.size() > 1) {
+			queue.poll();
+			queue.offer(queue.poll());
+		}
+		
+		System.out.println(queue.poll());
+	}
+}
