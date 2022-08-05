85719c9c2295813052917c2bdd1d8db0bfdc52ab
2HyeongSeok <rhfemsqpf90@naver.com>
2022-08-05 15:58:34 +0900

    feat: Add BOJ-2527-Java

---
+++ Live/src/boj/Main_2527_직사각형.java
@@ -0,0 +1,54 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_2527_직사각형 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int[] firstX = new int[2];
+		int[] firstY = new int[2];
+		int[] secondX = new int[2];
+		int[] secondY = new int[2];
+		for(int t = 1; t <= 4; t++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			firstX[0] = Integer.parseInt(st.nextToken());
+			firstY[0] = Integer.parseInt(st.nextToken());
+			firstX[1] = Integer.parseInt(st.nextToken());
+			firstY[1] = Integer.parseInt(st.nextToken());
+			secondX[0] = Integer.parseInt(st.nextToken());
+			secondY[0] = Integer.parseInt(st.nextToken());
+			secondX[1] = Integer.parseInt(st.nextToken());
+			secondY[1] = Integer.parseInt(st.nextToken());
+			
+			if(firstX[0] > secondX[1] || secondX[0] > firstX[1] || firstY[0] > secondY[1] || secondY[0] > firstY[1]) {
+				// 겹치지 않는 경우
+				sb.append("d").append("\n");
+			}else if((firstX[0] == secondX[1] && firstY[0] == secondY[1]) 
+					|| (firstX[1] == secondX[0] && firstY[0] == secondY[1]) 
+					|| (firstX[0] == secondX[1] && firstY[1] == secondY[0]) 
+					|| (firstX[1] == secondX[0] && firstY[1] == secondY[0])) {
+				// 한 점에서 만남
+				sb.append("c").append("\n");
+			}else if((firstX[0] == secondX[1] && ((firstY[1] > secondY[0] && firstY[1] <= secondY[1]) || (firstY[0] >= secondY[0] && firstY[0] < secondY[1])))
+					|| (firstX[1] == secondX[0] && ((firstY[1] > secondY[0] && firstY[1] <= secondY[1]) || (firstY[0] >= secondY[0] && firstY[0] < secondY[1])))
+					|| (firstY[0] == secondY[1] && ((firstX[1] > secondX[0] && firstX[1] <= secondX[1]) || (firstX[0] >= secondX[0] && firstX[0] < secondX[1])))
+					|| (firstY[1] == secondY[0] && ((firstX[1] > secondX[0] && firstX[1] <= secondX[1]) || (firstX[0] >= secondX[0] && firstX[0] < secondX[1])))) {
+				// 선분
+				sb.append("b").append("\n");
+			}else {
+				// 직사각형
+				sb.append("a").append("\n");
+			}
+		}
+		
+		System.out.println(sb);
+	}
+}
