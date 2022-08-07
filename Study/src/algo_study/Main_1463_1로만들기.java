5ae53ee2f61974f235f4c2466a15db38da936646
2HyeongSeok <rhfemsqpf90@naver.com>
2022-08-07 17:18:26 +0900

    feat: Fix BOJ-1463-Java

--- Study/src/algo_study/Main_1463_1로만들기.java
+++ Study/src/algo_study/Main_1463_1로만들기.java
@@ -5,57 +5,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main_1463_1로만들기 {
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int N = Integer.parseInt(br.readLine());
-		int res = 0;
-		int k = 4;
 		
-		int[] dp = new int[1000000]; // 최대
-		dp[0] = 0;
-		dp[1] = 1;
+		if(N == 1) {
+			System.out.println(0);
+			return;
+		} else if(N <= 3) {
+			// 2, 3이면 무조건 1회가 최소!
+			System.out.println(1);
+			return;
+		}
+		
+		int[] dp = new int[N + 1]; // 인덱스 맞추려고!
+		dp[1] = 0;
 		dp[2] = 1;
 		dp[3] = 1;
-		while(k < N) {
-			int temp = 0;
-			int temp2 = 0;
-			int temp3 = 0;
-			if(k % 3 == 0) {
-				temp3 = dp[k/3] + 1;
+		
+		for(int k = 4; k <= N; k++) {
+			dp[k] = dp[k - 1] + 1; // 미리 -- 하는 케이스로 저장해두기 *스터디팁*
+			if(k % 6 == 0) {
+				// 6으로 나누어지는 경우
+				dp[k] = calcMin(dp[k - 1], dp[k / 2], dp[k / 3]) + 1;
+			}else if(k % 3 == 0) {
+				// 3으로 나누어지는 경우
+				dp[k] = Math.min(dp[k - 1], dp[k / 3]) + 1;
+			}else if(k % 2 == 0) {
+				// 2로 나누어지는 경우
+				dp[k] = Math.min(dp[k - 1], dp[k / 2]) + 1;
 			}
-			if(k % 2 == 0) {
-				temp2 = dp[k/2] + 1;
-			}
-			int min = Math.min(temp2, temp3);
-			
-			temp = dp[k - 1] + 1;
-			min = Math.min(temp, min);
-			k++;
 		}
 		
 		System.out.println(dp[N]);
-//		if(N <= 3) {
-//			System.out.println(1);
-//			return;
-//		}
-//		while(N >= 4) {
-//			if(N % 3 == 0 && N % 2 == 0) {
-//				N /= 6;
-//				res ++; // 두번 나누는 것과 같으므로 한 번 더 ++!
-//			}else if(N % 3 == 0) {
-//				N /= 3;
-//			}else if(N % 2 == 0) {
-//				N /= 2;
-//			}else {
-//				N--;
-//			}
-//			res++;
-//			System.out.println(N);
-//		}
-//		res++; //while문 탈출하면 1인 경우는 없으므로 +1
-//		
-//		System.out.println(res);
+	}
+	
+	static int calcMin(int a, int b, int c) {
+		if(a >= c && b >= c) return c;
+		else if(a >= b && c >= b) return b;
+		else return a;
 	}
 }
