0320ad01db7535a06cb689e8ca1d9c313c043345
2HyeongSeok <rhfemsqpf90@naver.com>
2022-08-14 17:30:46 +0900

    feat: Add BOJ-1012, 1074-Java

---
+++ class03/src/class03/Main_1012_유기농배추.java
@@ -0,0 +1,83 @@
+package class03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_1012_유기농배추 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		int M, N, K;
+		
+		int[][] map;
+		boolean[][] checkedMap;
+		int[] dr = {-1, 1, 0, 0};
+		int[] dc = {0, 0, -1, 1};
+		Queue<Integer[]> queue;
+		
+		int counter = 0;
+		int nr, nc;
+		
+		for(int t = 0; t < T; t++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			
+			map = new int[M][N];
+			checkedMap = new boolean[M][N];
+			for(int k = 0; k < K; k++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				map[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
+			}
+			
+			queue = new ArrayDeque<>();
+			counter = 0;
+			
+			for(int i = 0; i < M; i++) {
+				for(int j = 0; j < N; j++) {
+					if(map[i][j] == 1 && !checkedMap[i][j]) { // 배추흰지렁이 필요하므로 bfs 시작
+						checkedMap[i][j] = true;
+						
+						for(int k = 0; k < 4; k++) {
+							nr = i + dr[k];
+							nc = j + dc[k];
+							
+							if(nr < 0 || nr >= M || nc < 0 || nc >= N) continue;
+							
+							if(map[nr][nc] == 1 && !checkedMap[nr][nc]) {
+								queue.offer(new Integer[] {nr, nc});
+								checkedMap[nr][nc] = true;
+							}
+						}
+						
+						while(!queue.isEmpty()) { // bfs
+							Integer[] temp = queue.poll();
+							for(int k = 0; k < 4; k++) {
+								nr = temp[0] + dr[k];
+								nc = temp[1] + dc[k];
+								
+								if(nr < 0 || nr >= M || nc < 0 || nc >= N) continue;
+								
+								if(map[nr][nc] == 1 && !checkedMap[nr][nc]) {
+									queue.offer(new Integer[] {nr, nc});
+									checkedMap[nr][nc] = true;
+								}
+							}
+						}
+						counter++;
+					}
+				}
+			}
+			sb.append(counter).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
---
+++ class03/src/class03/Main_1074_Z.java
@@ -0,0 +1,61 @@
+package class03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1074_Z {
+	static int count = 0;
+	static int maxRow;
+	static int maxCol;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		int size = (int)Math.pow(2, N);
+		maxRow = size;
+		maxCol = size;
+		
+		search(size, r, c);
+		
+		System.out.println(count);
+	}
+	
+	static void search(int size, int r, int c) { // 큰 사각형마다 4가지 케이스가 있으므로 재귀
+		if(size == 1) {
+			return;
+		}
+		
+		size /= 2;
+		int tempCount = (int)Math.pow(size, 2);
+		maxRow /= 2;
+		maxCol /= 2;
+		
+		if(r < maxRow && c < maxCol) {
+			search(size, r, c);
+		}else if(r < maxRow && c >= maxCol) {
+			c -= size;	
+			count += tempCount;
+			
+			search(size, r, c);
+		}else if(r >= maxRow && c < maxCol) {
+			r -= size;
+			count += tempCount * 2;
+			
+			search(size, r, c);
+		}else {
+			// r, c : 재귀에서 부른 다음 사각형에서는 index가 절반으로 줄기 때문에 size만큼 빼줌
+			r -= size; 
+			c -= size;
+			count += tempCount * 3; // 누적해서 더해주는 값
+						
+			search(size, r, c);
+		}
+	}
+}
