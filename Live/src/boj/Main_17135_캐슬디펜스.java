d1c92e525f4e196bc819195c8e5cdaea3b625364
2HyeongSeok <rhfemsqpf902naver.com>
2022-08-19 17:51:32 +0900

    feat: Add BOJ-17135(not yet)-Java

---
+++ Live/src/boj/Main_17135_캐슬디펜스.java
@@ -0,0 +1,134 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_17135_캐슬디펜스 {
+
+	static int[] selected;
+	static int[][] map;
+	static int N, M, D;
+	static int killCount = 0, enemyCount = 0, totalEnemy = 0, maxKillCount = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		D = Integer.parseInt(st.nextToken());
+
+		map = new int[N + 1][M];
+		int[][] tempMap = new int[N + 1][M];
+		selected = new int[3];
+
+		totalEnemy = 0;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < M; j++) {
+				int value = Integer.parseInt(st.nextToken());
+				map[i][j] = value;
+				tempMap[i][j] = value;
+				totalEnemy += map[i][j];
+			}
+		}
+
+		comb(tempMap, 0, 0);
+
+		System.out.println(maxKillCount);
+	}
+
+	static void comb(int[][] tempMap, int cnt, int start) {
+		if (cnt == 3) {
+//			System.out.println(Arrays.toString(selected));
+			for (int i = 0; i < N + 1; i++) {
+				for (int j = 0; j < M; j++) {
+					map[i][j] = tempMap[i][j];
+				}
+			}
+
+			enemyCount = 0;
+			killCount = 0;
+
+			while (enemyCount + killCount != totalEnemy) {
+				attack();
+				move();
+			}
+			maxKillCount = maxKillCount > killCount ? maxKillCount : killCount;
+
+			return;
+		}
+
+		for (int i = start; i < M; i++) {
+			selected[cnt] = i;
+			comb(tempMap, cnt + 1, i + 1);
+		}
+	}
+
+	static void attack() {
+		ArrayList<Integer[]> rowColList = new ArrayList<>();
+		int distance = N + 1, left = M + 1, row = -1;
+		for (int i = 0; i < 3; i++) {
+			distance = N + 1; // 가장 멀다고 가정
+			left = M + 1; // 가장 오른쪽 시작 가정
+			row = -1; // 가장 위 가정
+
+			for (int m = N - 1; m >= 0; m--) {
+				for (int n = M - 1; n >= 0; n--) {
+					if (map[m][n] == 1) {
+						// 적과 i번째 궁수와의 거리
+						int tempD = Math.abs(N - m) + Math.abs(selected[i] - n);
+						if (tempD <= D) {
+							if (tempD <= distance) {
+								distance = tempD; // 가장 가까운 적 중에서
+								if (n < left) {
+									row = m;
+									left = n; // 가장 왼쪽 찾기
+								}
+							}
+						}
+					}
+				}
+			}
+			if (row != -1 && left != M + 1)
+				rowColList.add(new Integer[] { row, left });
+
+		}
+
+		// 찾은 적 제거
+		for (int i = 0; i < rowColList.size(); i++) {
+			if (map[rowColList.get(i)[0]][rowColList.get(i)[1]] == 1) {
+				map[rowColList.get(i)[0]][rowColList.get(i)[1]] = 0;
+				killCount++;
+			}
+		}
+	}
+
+	static void move() { // 한 칸 밑으로
+		for (int i = 0; i < M; i++) {
+			if (map[N - 1][i] == 1)
+				enemyCount++; // 사라질 애들 카운팅
+		}
+
+		for (int i = N - 2; i >= 0; i--) {
+			for (int j = 0; j < M; j++) {
+				// 어차피 N-1번째 줄은 성에 도달해서 사라질 예정이므로
+				map[i + 1][j] = map[i][j];
+			}
+		}
+
+		for (int i = 0; i < M; i++) {
+			map[0][i] = 0;
+		}
+
+//		for(int i = 0; i < N; i++) {
+//			System.out.println(Arrays.toString(map[i]));
+//		}
+//		System.out.println();
+	}
+}
--- Live/src/swea/d4/Solution_3234_준환이의양팔저울.java
+++ Live/src/swea/d4/Solution_3234_준환이의양팔저울.java
@@ -3,7 +3,6 @@ package swea.d4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution_3234_준환이의양팔저울 {
