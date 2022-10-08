584c74419e559e32c0e08d19d78559ef80aa9341
2HyeongSeok <rhfemsqpf90@naver.com>
2022-10-08 18:19:24 +0900

    feat: Add BOJ-11403-경로찾기

---
+++ Study/src/algo_study/Main_S1_11403_경로찾기.java
@@ -0,0 +1,42 @@
+package algo_study;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_S1_11403_경로찾기 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 0) map[i][j] = 100000;
+			}
+		}
+		
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+				}
+			}
+		}
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				if(map[i][j] >= 100000) map[i][j] = 0;
+				else map[i][j] = 1;
+				
+				sb.append(map[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
