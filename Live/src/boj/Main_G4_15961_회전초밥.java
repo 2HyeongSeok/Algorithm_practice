6124082c6f9147f5886da015f9c80959f7b1f4f4
2HyeongSeok <rhfemsqpf90@naver.com>
2022-10-11 20:18:52 +0900

    feat: Fix BOJ-15961-회전초밥(배열)

--- Live/src/boj/Main_G4_15961_회전초밥.java
+++ Live/src/boj/Main_G4_15961_회전초밥.java
@@ -14,35 +14,29 @@ public class Main_G4_15961_회전초밥 {
 		int k = Integer.parseInt(st.nextToken()); // 연속해서 먹는 접시 수
 		int c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
 		
-		Map<Integer, Integer> numbering = new HashMap<>();
-		Map<Integer, Integer> counter = new HashMap<>();
+		int[] numbers = new int[N];
+		int[] counting = new int[d+1];
 		
 		for(int i = 0; i < N; i++) {
-			numbering.put(i, Integer.parseInt(br.readLine()));
+			numbers[i] = Integer.parseInt(br.readLine());
 		}
 		
 		int maxLength = 0;
 		int s = 0, e = 0, real_e = 0, length = 0; // 투포인터 사용
 		while(s < N) {
 			if(e - s < k) {
-				int value = counter.getOrDefault(numbering.get(real_e), 0);
-				if(value == 0) length++; // value가 0이라면 새롭게 추가 됐으므로 길이도 더함
-				counter.put(numbering.get(real_e), value + 1);
+				if(counting[numbers[real_e]] == 0) length++; // value가 0이라면 새롭게 추가 됐으므로 길이도 더함
+				counting[numbers[real_e]]++;
 				e++;
 				real_e++;
 			}else if(e - s == k) {
 				// 종류 수 갱신하고
-				if(counter.getOrDefault(c, 0) > 0) maxLength = maxLength > length ? maxLength : length;
+				if(counting[c] >= 1) maxLength = maxLength > length ? maxLength : length;
 				else maxLength = maxLength > length + 1 ? maxLength : length + 1;
 				
 				// 가장 오래된거 제거하고 s증가
-				int value = counter.getOrDefault(numbering.get(s), 0);
-				if(value == 1) {
-					length--; // value가 1이라면 마지막 원소이므로 삭제될 때 길이도 빼야함
-					counter.remove(numbering.get(s));
-				}else {
-					counter.put(numbering.get(s), value - 1);					
-				}
+				if(counting[numbers[s]] == 1) length--; // value가 1이라면 마지막 원소이므로 삭제될 때 길이도 빼야함
+				counting[numbers[s]]--;
 				s++;
 			}
 			
